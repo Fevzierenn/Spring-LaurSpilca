530ce916d53ea45698a5a295c9a01203e742fc1f
Fevzi Eren Çiçek <fevzierencicek@gmail.com>
2023-12-01 02:19:56 +0300

    Update Product.java

--- Lesson12-BasicContratsinSpringData_SortPagination-e1/src/main/java/com/LaurentSpilca/Lesson12BasicContratsinSpringData/entities/Product.java
+++ Lesson12-BasicContratsinSpringData_SortPagination-e1/src/main/java/com/LaurentSpilca/Lesson12BasicContratsinSpringData/entities/Product.java
@@ -30,36 +30,4 @@ public class Product {
 
 
 }
-=======
-package com.LaurentSpilca.Lesson12BasicContratsinSpringData.entities;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "Product")    //class ismi ile table ismi aynı olursa bu tanımı yapmaya gerek kalmaz
-public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
-
-    @Column(name = "name")
-    private String name;
-
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-
-}
->>>>>>> faf4d25201ab6c05f7fbb8d3b5a36fafd745fbbe
